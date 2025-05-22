b30d0864575476be21a21834dc226cad360e110b
axelb <axel.tlth.se>
2025-05-22 19:52:23 +0200

    started making backup function

--- makeBackupDB.py
+++ makeBackupDB.py
@@ -1,7 +1,9 @@
 import shutil
 
 source_path = 'hilbertDatabase.db'
-backup_path = 'hilbertDatabase_backup.db'
+backup_path = 'hilbertDatabase_backup(2025-05-22).db'
 
-shutil.copy2(source_path, backup_path)
+# shutil.copy2(source_path, backup_path)
+# shutil.move("")
+print(__file__)
 print("Backup created successfully!")
